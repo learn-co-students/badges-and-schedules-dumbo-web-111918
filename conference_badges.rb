8e50272fc626cc105f9d9dd0c98c2ee4eda7e57a
Albina Zakirova <zak1804@gmail.com>
2018-11-09 16:13:05 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,15 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(attendees)
+  counter = 1
+  while counter < attendees.size 
+    return badge_maker(attendees[counter-1])
+    counter + = 1
+  end
+end
+
+def assign_rooms
+  
+end
