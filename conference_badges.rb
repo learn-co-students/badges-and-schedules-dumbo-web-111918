4ca2007ee628579aa70813e49ac2fd1d28c8a06d
Benson Yu <bennywhyu@gmail.com>
2018-11-12 02:53:07 +0000

    Done.

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,29 @@
 # Write your code here.
+def badge_maker(name)
+  return "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(array)
+  new_array=[]
+  array.each{|x|
+    new_array.push("Hello, my name is #{x}.")
+  }
+  return new_array
+end
+
+def assign_rooms(array)
+  new_array=[]
+  array.each{|x|
+    new_array.push("Hello, #{x}! You'll be assigned to room #{array.index(x)+1}!")
+  }
+  return new_array
+end
+
+def printer(attendees)
+  batch_badge_creator(attendees).each{|x|
+    puts x
+  }
+  assign_rooms(attendees).each{|y|
+    puts y
+  }
+end
